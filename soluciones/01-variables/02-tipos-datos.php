6bf2e7d793ea59cbeee817d567e38ec0fbb25c77
Ana Delia <masteringfrontend360@gmail.com>
2026-04-01 12:29:11 +0200

    Actualizar configuración

--- soluciones/01-variables/02-tipos-datos.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Ejercicio 2
-// 1. Define una variable de tipo entero
-// 2. Define una variable de tipo flotante
-// 3. Define una variable de tipo cadena
-// 4. Define una variable de tipo booleano
-// 5. Define una variable de tipo array
-// 6. Muestra el tipo y el valor con var_dump()
