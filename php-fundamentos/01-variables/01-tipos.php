978831f91a885befe2aa3228044f994795bc8f44
Sergio <sergiofrontenddeveloper@gmail.com>
2026-04-08 21:21:37 +0200

    ejercicios php variables completados

--- php-fundamentos/01-variables/01-tipos.php
+++ php-fundamentos/01-variables/01-tipos.php
@@ -7,4 +7,27 @@
 // 5. Usa var_dump() para ver el tipo de cada variable
 
 
-// dXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+// 1. Declara 3 variables de distinto tipo
+$cancion = "Ain´t No Sunshine";
+$artista = "Bill Withers";
+$album = "Just As I Am";
+
+// 2. Crea una constante con tu nombre
+const SERGIO = [
+    "nombre" => "Sergio",
+    "apellido1" => "Martinez",
+    "apellido2" => "Aramburu",
+    "edad" => 31,
+    "origen" => "Zaragoza"
+];
+
+// 3. Haz una suma y muéstrala con echo
+$a = 25;
+$b = 50;
+$suma_ab = $a + $b;
+
+echo $suma_ab;
+
+// 4. Compara dos valores con == y con ===
+2 == "2";
+2 === "2";
