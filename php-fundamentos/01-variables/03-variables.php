9aa6ac51ea5261e5fdedcc4ea3d5cb543f6286d5
Mayckel <techprogramation.07@gmail.com>
2026-04-09 13:32:21 +0200

    realizacion del ejercio 03-variables / cambio de varibles sin mas ni menos

--- php-fundamentos/01-variables/03-variables.php
+++ php-fundamentos/01-variables/03-variables.php
@@ -2,7 +2,18 @@
 
 // Ejercicio 3
 // 1. Declara una variable entera y una cadena de texto
+$entero = 777;
+$cadena = "hola, me mude al file 03-variables.php";
+
 // 2. Muestra sus valores iniciales
+echo "valor inicial de la variable entera: " . $entero . "<br>";
+echo "valor inicial de la variable cadena: " . $cadena . "<br>";
+
 // 3. Cambia el valor de la variable entera
+$entero = 888;
+
 // 4. Concatena texto a la cadena
+$cadena .= "ahora tiene un valor diferente, viva 777";
+
 // 5. Muestra los valores finales
+echo "ahora mostramos los valores finales: " . $entero . "<br>" . $cadena;
