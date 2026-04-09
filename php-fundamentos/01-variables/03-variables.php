979fe598051e83ab19b5523afae649449774a936
Sergio <smartineza@SAPC26AINFA304.alumnos.zaragozadinamica.es>
2026-04-09 12:06:35 +0200

    Ejercicios completados

--- php-fundamentos/01-variables/03-variables.php
+++ php-fundamentos/01-variables/03-variables.php
@@ -6,3 +6,21 @@
 // 3. Cambia el valor de la variable entera
 // 4. Concatena texto a la cadena
 // 5. Muestra los valores finales
+
+// 1. Declara una variable entera y una cadena de texto
+$edad = 32;
+$nombre = "Sergio";
+
+// 2. Muestra sus valores iniciales
+echo $edad;
+echo $nombre;
+
+// 3. Cambia el valor de la variable entera
+$edad = 31;
+
+// 4. Concatena texto a la cadena
+$edad .= $nombre;
+echo $edad;
+
+// 5. Muestra los valores finales
+echo $edad;
