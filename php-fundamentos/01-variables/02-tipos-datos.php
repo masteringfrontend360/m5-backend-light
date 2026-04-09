213ae1d2ac5871e46983dcb6a07e52a0658272a4
Sergio <smartineza@SAPC26AINFA304.alumnos.zaragozadinamica.es>
2026-04-09 10:34:40 +0200

    Ejercicios completados

--- php-fundamentos/01-variables/02-tipos-datos.php
+++ php-fundamentos/01-variables/02-tipos-datos.php
@@ -7,3 +7,22 @@
 // 4. Define una variable de tipo booleano
 // 5. Define una variable de tipo array
 // 6. Muestra el tipo y el valor con var_dump()
+
+
+// 1. Define una variable de tipo entero
+$edad = 32;
+
+// 2. Define una variable de tipo flotante
+$altura = 1.78;
+
+// 3. Define una variable de tipo cadena
+$nombre = "Sergio Martinez Aramburu";
+
+// 4. Define una variable de tipo booleano
+$empleado = false;
+
+// 5. Define una variable de tipo array
+$alumnos = ["Sergio", "Mayckel", "Ana"];
+
+// 6. Muestra el tipo y el valor con var_dump()
+var_dump($alumnos);
