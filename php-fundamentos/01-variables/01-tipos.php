affd0ed67441ce632da7b1374d0c599dcf212482
anavallejorivera <avallejorivera360@gmail.com>
2026-04-09 08:41:28 +0200

    primero ejercicio

--- php-fundamentos/01-variables/01-tipos.php
+++ php-fundamentos/01-variables/01-tipos.php
@@ -1,7 +1,27 @@
 <?php
 // Ejercicio 1
 // 1. Declara 3 variables de distinto tipo
+$numero = 5;
+$decimal = 3.14;
+$cadena = "Hola, soy una cadena de texto";
+
 // 2. Crea una constante con tu nombre
+define("NOMBRE", "Tu Nombre");
+
 // 3. Haz una suma y muéstrala con echo
+$suma = $numero + $decimal;
+echo "La suma es: " . $suma . "<br>";
+
 // 4. Compara dos valores con == y con ===
+$valor1 = 5;
+$valor2 = "5";
+echo "Comparación con ==: " . ($valor1 == $valor2 ? "Iguales" : "Diferentes") . "<br>";
+echo "Comparación con ===: " . ($valor1 === $valor2 ? "Iguales" : "Diferentes") . "<br>";
+
 // 5. Usa var_dump() para ver el tipo de cada variable
+var_dump($numero);
+var_dump($decimal);
+var_dump($cadena);
+var_dump(NOMBRE);
+
+?>
