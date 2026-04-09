91d383e6cf122cc897c562b6943063dbe6fe5d4a
Mayckel <techprogramation.07@gmail.com>
2026-04-09 12:42:54 +0200

    finalizacion del ejercicio 01-tipos / uso de variables, suma, comparacion y visualizacion del tipo de la variable

--- php-fundamentos/01-variables/01-tipos.php
+++ php-fundamentos/01-variables/01-tipos.php
@@ -1,23 +1,33 @@
+<h1>Ejercicio variables de tipos</h1>
 <?php
 // Ejercicio 1
 // 1. Declara 3 variables de distinto tipo
+
+$nombre = "mayckel";
+$edad = 18;
+$altura = 1.82;
+
 // 2. Crea una constante con tu nombre
+
+const nombre_completo = "mayckel araujo";
+
 // 3. Haz una suma y muéstrala con echo
+
+$m = 18;
+$n = 12;
+$e = $m + $n;
+$suma = $m + $n;
+echo $e . "<br>";
+echo $suma . "<br> <br>";
+
 // 4. Compara dos valores con == y con ===
+
+$año = 2024;
+$numero = "2024";
+
+var_dump($año == $numero);
+var_dump($año === $numero);
+
 // 5. Usa var_dump() para ver el tipo de cada variable
-// Prueba RAma mayckel
 
-<?php 
-$entero = 5;
-$flotante = 3.14;
-$cadena = "Hola, mundo!";
-const NOMBRE = "Mayckel";
-
-$suma = $entero + $flotante;
-echo $suma . "\n";
-
-var_dump($entero);
-var_dump($flotante);
-var_dump($cadena);
-var_dump(NOMBRE);
-?>
+var_dump($nombre, $edad, $altura);
