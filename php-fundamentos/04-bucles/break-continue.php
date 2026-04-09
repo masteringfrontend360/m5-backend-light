bee5bbda4d714b1ad25e96ca1cbff1c93829dda9
autanaweb <autanafrontend@gmail.com>
2026-04-09 23:59:19 +0200

    break && do-while

--- php-fundamentos/04-bucles/break-continue.php
+++ php-fundamentos/04-bucles/break-continue.php
@@ -5,9 +5,28 @@
 // Usar un bucle for para encontrar el primer número divisible por 7
 // entre 1 y 100, y detener la ejecución con break al encontrarlo.
 
-// Ejercicio 2:
+for ($i = 1; $i <= 100; $i++) {
+    if ($i % 7 == 0) {
+        echo "El primer número divisible por 7 es: $i";
+        break;
+    }
+}
+
+echo "<br><br>";
+ // Ejercicio 2:
 // Usar un bucle for para mostrar los números del 1 al 20,
 // saltando los múltiplos de 3 con continue.
+
+echo "<br> Números del 1 al 20 (saltando múltiplos de 3): <br>";
+
+for ($i = 1; $i <= 20; $i++) {
+    if ($i % 3 == 0) {
+        continue;
+    }
+    echo $i . " ";
+}
+
+
 
 /*
 Guía:
--- php-fundamentos/04-bucles/do-while.php
+++ php-fundamentos/04-bucles/do-while.php
@@ -3,8 +3,46 @@
 
 // Ejercicio 1: Mostrar todos los números múltiplos de 15
 // entre 30 y 75, ambos incluidos.
+$i = 30;
+
+echo "Múltiplos de 15 entre 30 y 75:<br>";
+
+do {
+    if ($i % 15 == 0) {
+        echo $i . " ";
+    }
+    $i++;
+} while ($i <= 75);
+echo "<br><br>";
 
 // Ejercicio 2: Sumar todos los números impares entre 0 y 10.
 
+$i = 0;
+$suma = 0;
+
+do {
+    if ($i % 2 != 0) {
+        $suma += $i;
+    }
+    $i++;
+} while ($i <= 10);
+
+echo "La suma de los números impares entre 0 y 10 es: $suma";
+
 // Ejercicio 3: Mostrar la tabla de multiplicar del 2,
 // del 1 al 10, dentro de una tabla HTML.
+$i = 1;
+
+echo "<h3>Tabla del 2</h3>";
+echo "<table border='1' cellpadding='5'>";
+
+do {
+    $resultado = 2 * $i;
+    echo "<tr>
+            <td>2 x $i</td>
+            <td>$resultado</td>
+          </tr>";
+    $i++;
+} while ($i <= 10);
+
+echo "</table>";
