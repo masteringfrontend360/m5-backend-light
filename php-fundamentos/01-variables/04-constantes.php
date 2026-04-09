912a826d83a71e1049daec7a091b694f2c94111b
Sergio <smartineza@SAPC26AINFA304.alumnos.zaragozadinamica.es>
2026-04-09 13:30:52 +0200

    Ejercicios completados hasta X

--- php-fundamentos/01-variables/04-constantes.php
+++ php-fundamentos/01-variables/04-constantes.php
@@ -5,3 +5,5 @@
 // 2. Define la constante TIENDA con el nombre de tu tienda
 // 3. Calcula el precio final de un producto de 100
 // 4. Muestra el nombre de la tienda y el precio final
+
+// 1. Define la constante IVA con valor 0.21
