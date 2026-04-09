de047298c43312b9939fe697c5a08d242c0f06e1
Mayckel <techprogramation.07@gmail.com>
2026-04-09 11:53:06 +0200

    ejercicio 01-tipos realizado con sumas

--- php-fundamentos/01-variables/01-tipos.php
+++ php-fundamentos/01-variables/01-tipos.php
@@ -6,3 +6,18 @@
 // 4. Compara dos valores con == y con ===
 // 5. Usa var_dump() para ver el tipo de cada variable
 // Prueba RAma mayckel
+
+<?php 
+$entero = 5;
+$flotante = 3.14;
+$cadena = "Hola, mundo!";
+const NOMBRE = "Mayckel";
+
+$suma = $entero + $flotante;
+echo $suma . "\n";
+
+var_dump($entero);
+var_dump($flotante);
+var_dump($cadena);
+var_dump(NOMBRE);
+?>
