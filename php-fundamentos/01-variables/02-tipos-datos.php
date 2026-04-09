4076fcfda5f2b732e2c34d06b094996cfb824b23
Mayckel <techprogramation.07@gmail.com>
2026-04-09 13:11:08 +0200

    realizacion del ejercicio 02-tipos de datos / se escribio cada uno de los tipo de variables que hay y luego se visualizan a traves de un var_dump

--- php-fundamentos/01-variables/02-tipos-datos.php
+++ php-fundamentos/01-variables/02-tipos-datos.php
@@ -2,8 +2,20 @@
 
 // Ejercicio 2
 // 1. Define una variable de tipo entero
+$entero = 777;
+
 // 2. Define una variable de tipo flotante
+$flotante = 7.77;
+
 // 3. Define una variable de tipo cadena
+$cadena = "hola, soy una cadena de texto o string 777";
+
 // 4. Define una variable de tipo booleano
+$booleano = true;
+
 // 5. Define una variable de tipo array
+$array = [1, "2", 3.0, false, "hola, 777"];
+
 // 6. Muestra el tipo y el valor con var_dump()
+
+var_dump("tipo y valor de la variable:", $entero, $flotante, $cadena, $booleano, $array);
