92833c10fc2575e45e0d85487cb435829a61c71e
Mayckel <techprogramation.07@gmail.com>
2026-04-10 08:45:03 +0200

    trabajando con constantes / elaborando suma

--- php-fundamentos/01-variables/04-constantes.php
+++ php-fundamentos/01-variables/04-constantes.php
@@ -2,6 +2,15 @@
 
 // Ejercicio 4
 // 1. Define la constante IVA con valor 0.21
+const IVA = 0.21;
+
 // 2. Define la constante TIENDA con el nombre de tu tienda
+const tienda = 'Eneba';
+
 // 3. Calcula el precio final de un producto de 100
+$precio = 100;
+$preciofinal = $precio + ($precio * IVA);
+
 // 4. Muestra el nombre de la tienda y el precio final
+echo 'nombre de la tienda: ' . tienda . '<br>';
+echo 'precio final del producto: ' . $preciofinal;
--- php-fundamentos/01-variables/05-operaciones-aritmeticas.php
+++ php-fundamentos/01-variables/05-operaciones-aritmeticas.php
@@ -2,8 +2,18 @@
 
 // Ejercicio 5
 // 1. Declara dos números
+
+
 // 2. Realiza suma
+
+
 // 3. Realiza resta
+
+
 // 4. Realiza multiplicación
+
+
 // 5. Realiza división
+
+
 // 6. Muestra todos los resultados
