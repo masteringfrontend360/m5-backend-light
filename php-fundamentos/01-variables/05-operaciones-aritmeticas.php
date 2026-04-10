1b2daf96da6c85695f58245c37f7da12d2b9383d
anavallejorivera <avallejorivera360@gmail.com>
2026-04-10 12:21:57 +0200

    Ejercicios Variables Ana Vallejo

--- php-fundamentos/01-variables/05-operaciones-aritmeticas.php
+++ php-fundamentos/01-variables/05-operaciones-aritmeticas.php
@@ -2,8 +2,18 @@
 
 // Ejercicio 5
 // 1. Declara dos números
+$num1 = 10;
+$num2 = 5;
 // 2. Realiza suma
+$suma = $num1 + $num2;
 // 3. Realiza resta
+$resta = $num1 - $num2;
 // 4. Realiza multiplicación
+$multiplicacion = $num1 * $num2;
 // 5. Realiza división
+$division = $num1 / $num2;
 // 6. Muestra todos los resultados
+echo "Suma: " . $suma . "<br>";
+echo "Resta: " . $resta . "<br>";
+echo "Multiplicación: " . $multiplicacion . "<br>";
+echo "División: " . $division . "<br>";
--- php-fundamentos/01-variables/06-incremento-decremento.php
+++ php-fundamentos/01-variables/06-incremento-decremento.php
@@ -2,8 +2,19 @@
 
 // Ejercicio 6
 // 1. Declara una variable $numero con valor inicial 5
+$numero = 5;
+echo "Valor inicial: " . $numero . "<br>";
 // 2. Realiza pre-incremento
-// 3. Realiza post-incremento
+++$numero;
+echo "Valor después de pre-incremento: ". $numero . "<br>";
+// 3. Realiza post-incremento 
+$numero++ ;
+echo "Valor después del post-incremento:". $numero . "<br>";
 // 4. Realiza pre-decremento
+--$numero;
+echo "Valor después de pre-decremento: " . $numero . "<br>";
 // 5. Realiza post-decremento
+ $numero-- ;
+echo "Valor después del post-decremento: $numero<br>";
 // 6. Muestra los resultados en cada paso
+
