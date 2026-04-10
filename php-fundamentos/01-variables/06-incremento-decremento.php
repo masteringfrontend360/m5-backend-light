25f86c0d29ff961ef95d74496d4eb0192b66acfb
Mayckel <techprogramation.07@gmail.com>
2026-04-10 13:17:43 +0200

    realizacion de ejercicios de pre/post-incremento/-decremento

--- php-fundamentos/01-variables/06-incremento-decremento.php
+++ php-fundamentos/01-variables/06-incremento-decremento.php
@@ -2,8 +2,27 @@
 
 // Ejercicio 6
 // 1. Declara una variable $numero con valor inicial 5
+$numero = 5;
+
 // 2. Realiza pre-incremento
+++$numero;
+echo $numero . '<br><br>';
+
 // 3. Realiza post-incremento
+$numero++;
+echo $numero . '<br><br>';
+
 // 4. Realiza pre-decremento
+--$numero;
+echo $numero . '<br><br>';
+
 // 5. Realiza post-decremento
+$numero--;
+echo $numero . '<br><br>';
+
 // 6. Muestra los resultados en cada paso
+// echo 'incremento y decremento con la variable ' . $numero . '<br><br>';
+// echo 'pre-incremento: ' . ++$numero . '<br><br>';
+// echo 'post-incremento: ' . $numero++ . '<br><br>';
+// echo 'pre-decremento: ' . --$numero . '<br><br>';
+// echo 'post-decremento: ' . $numero-- . '<br><br>';
